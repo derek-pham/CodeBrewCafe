863328523887da11f55c01c3bdf0d9d214fe5048
derek-pham <derekpham3@gmail.com>
2024-06-01 14:26:51 +1000

    - implemented button for simonsays

--- src/pages/HomePage/components/MainMenu/MainMenu.tsx
+++ src/pages/HomePage/components/MainMenu/MainMenu.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './MainMenu.css';
 import TicTacToeButton from './buttons/TicTacToeButton/TicTacToeButton';
 import CalculatorButton from './buttons/CalculatorButton/CalculatorButton';
+import SimonSaysButton from './buttons/SimonSaysButton/SimonSaysButton';
 
 function MainMenu() {
   return (
     <div className="mainMenu">
       <TicTacToeButton />
       <CalculatorButton />
+      <SimonSaysButton />
       <button id='testbutton'></button>
       <button id='testbutton'></button>
       <button id='testbutton'></button>
---
+++ src/pages/HomePage/components/MainMenu/buttons/SimonSaysButton/SimonSaysButton.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useContext } from 'react';
+import './SimonSaysButton.css';
+import simonSaysImage from './simonsays.png'
+import { HomePageContext } from '../../../../HomePageContext';
+
+function SimonSaysButton() {
+
+    const { isSelected, setIsSelected, setDisplayTitle, setDisplayImage, setDescription, setPageLink } = useContext(HomePageContext)
+
+    const description = 'Challenge your memory and reflexes with Simon Says! Follow the ever-growing sequences of colored lights and sounds—can you keep up and master the game?'
+
+    const handleClick = () => {
+        setIsSelected('simonSays');
+        setDisplayTitle('SIMON SAYS')
+        setDisplayImage(simonSaysImage)
+        setDescription(description)
+        setPageLink('#')
+    };
+
+    return (
+        <button
+            className={`simonSaysButton ${isSelected == 'simonSays' ? 'simonSaysButton-selected' : ''}`}
+            onClick={handleClick}
+        >
+        </button>
+    );
+}
+
+export default SimonSaysButton;
