15ef59da82e6920257c151d5fbc913bd9baee2ae
derek-pham <derekpham3@gmail.com>
2024-06-04 12:59:15 +1000

    - refined play button

--- src/pages/SimonSaysPage/components/SimonSaysBottom/components/SimonSaysPlaySeq/SimonSaysPlaySeq.tsx
+++ src/pages/SimonSaysPage/components/SimonSaysBottom/components/SimonSaysPlaySeq/SimonSaysPlaySeq.tsx
@@ -1,23 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './SimonSaysPlaySeq.css'
 import { SSContext } from "../../../../SimonSaysPageContext";
 
 export default function SimonSaysPlaySeq() {
     const { delay, sequencePattern, setActiveSquare } = useContext(SSContext)
+    const [isDisabled, setIsDisabled] = useState(false)
 
     async function playSequence() {
         console.log(sequencePattern)
+        setIsDisabled(true)
         for (let i = 0; i < sequencePattern.length; i++) {
             let x = sequencePattern[i]
             setActiveSquare(`sq${x}`)
             console.log(x)
             await delay(1100);
         }
+        setIsDisabled(false)
     }
 
     return (
-        <div className="simonsaysplayseq">
-            <button onClick={playSequence}>▶️</button>
+        <div className={`simonsaysplayseq ${isDisabled&& 'disabled'}`}>
+            <button id="ssplaybutton" onClick={playSequence}>PLAY</button>
         </div>
     )
 }
